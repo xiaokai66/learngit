b2ac35b1e0fbbd1432e4c29a61ef4957ca55461a
root <root@localhost.localdomain>
2016-08-15 09:41:51 +0800

    two

---
+++ index.php
@@ -0,0 +1,22 @@
+<!--http://tts.baidu.com/text2audio?lan=zh&ie=UTF-8&spd=2&text=%E4%BD%AO%E8%A6%81%E8%BD%AC%E6%8D%A2%E7%9A%84%E6%8D%A2%E7%9A%84%E6%96%87%E5%AD%97-->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>提交页面</title>
+</head>
+<body>
+<form action="demo.php" method="post">
+    <table>
+        <tr>
+            <td>要转换的文字</td>
+            <td><input type="text" name="name"/></td>
+        </tr>
+        <tr>
+            <td><input type="submit" value="提交"/></td>
+        </tr>
+    </table>
+</form>
+</body>
+</html>
